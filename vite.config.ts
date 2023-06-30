e1392f93ddb6db687e924febe562387fb5aa5826
Adam Gąsowski <adam.gasowski@gander.pl>
2023-06-30 16:28:47 +0200

    _worker.js fix?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        worker: fileURLToPath(new URL('./_worker.js', import.meta.url)),
+      }
+    }
+  },
   plugins: [
     vue(),
   ],
