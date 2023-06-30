a4e2d744acde0eb500c013ef03ae00a21f9542f2
Adam Gąsowski <adam.gasowski@gander.pl>
2023-06-30 22:16:03 +0200

    /token/[sub]

--- functions/token-json.js
+++
@@ -1,8 +0,0 @@
-import * as jose from 'jose';
-
-export const onRequest = async (context) => {
-    const secret = new TextEncoder().encode(context.env.VITE_TOKEN_HMAC_SECRET_KEY);
-    const token = await new jose.SignJWT({sub: ""}).setProtectedHeader({alg: 'HS256'}).sign(secret);
-    return new Response(JSON.stringify({token}));
-};
-
---
+++ functions/token/[sub].ts
@@ -0,0 +1,12 @@
+import * as jose from 'jose';
+
+interface Env {
+    VITE_TOKEN_HMAC_SECRET_KEY: string;
+}
+
+export const onRequest: PagesFunction<Env> = async ({env: {VITE_TOKEN_HMAC_SECRET_KEY: secret_key}, params: {sub}}) => {
+    const secret = new TextEncoder().encode(secret_key);
+    const token = await new jose.SignJWT({sub}).setProtectedHeader({alg: 'HS256'}).sign(secret);
+    return new Response(JSON.stringify({sub, token}, null, 2));
+};
+
